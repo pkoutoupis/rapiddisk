7ee05633da11149e093f34fb5c28381fa57f918b
Petros Koutoupis <petros@petroskoutoupis.com>
2016-08-19 08:11:12 -0500

    Pushing 4.3.

--- module/rapiddisk-cache.c
+++ module/rapiddisk-cache.c
@@ -47,7 +47,7 @@
 	} \
 } while (0)
 
-#define VERSION_STR	"4.2"
+#define VERSION_STR	"4.3"
 #define DM_MSG_PREFIX	"rapiddisk-cache"
 
 #define READCACHE	1
@@ -1186,7 +1186,7 @@ cache_status(struct dm_target *ti, status_type_t type, unsigned status_flags,
 
 static struct target_type cache_target = {
 	.name    = "rapiddisk-cache",
-	.version = {4, 2, 0},
+	.version = {4, 3, 0},
 	.module  = THIS_MODULE,
 	.ctr	 = cache_ctr,
 	.dtr	 = cache_dtr,
