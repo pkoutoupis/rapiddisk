7bc5139466c21345690e7815db3e0f3b95e52c33
Boian Berberov <bberberov+github@gmail.com>
2016-10-31 16:14:43 -0500

    GPLv2 headers for /module, based on language from Facebook/flashcache_wt

--- module/rapiddisk.c
+++ module/rapiddisk.c
@@ -1,13 +1,23 @@
 /*******************************************************************************
- ** Copyright (c) 2011-2016 Petros Koutoupis
+ ** Copyright © 2011-2016 Petros Koutoupis
  ** All rights reserved.
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation; under version 2 of the License.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **
  ** filename: rapiddisk.c
  ** description: RapidDisk is an enhanced Linux RAM disk module to dynamically
  **	 create, remove, and resize non-persistent RAM drives.
  ** created: 1Jun11, petros@petroskoutoupis.com
- **
- ** This file is licensed under GPLv2.
  **
  ******************************************************************************/
 
