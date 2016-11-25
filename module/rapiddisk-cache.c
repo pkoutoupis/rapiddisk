ad2458c38584a1928e871fbdb714f45ae49c0b6a
Petros Koutoupis <petros@petroskoutoupis.com>
2016-11-25 17:01:42 -0600

    Updated version number.

--- module/rapiddisk-cache.c
+++ module/rapiddisk-cache.c
@@ -57,7 +57,7 @@
 	} \
 } while (0)
 
-#define VERSION_STR	"4.5"
+#define VERSION_STR	"5.0"
 #define DM_MSG_PREFIX	"rapiddisk-cache"
 
 #define READCACHE	1
@@ -1212,7 +1212,7 @@ cache_status(struct dm_target *ti, status_type_t type, unsigned status_flags,
 
 static struct target_type cache_target = {
 	.name    = "rapiddisk-cache",
-	.version = {4, 5, 0},
+	.version = {5, 0, 0},
 	.module  = THIS_MODULE,
 	.ctr	 = cache_ctr,
 	.dtr	 = cache_dtr,
