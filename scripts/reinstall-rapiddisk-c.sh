1503e0f41492d999e4cd3226a93fc326ee1df7a2
juanpc2018 <33388902+juanpc2018@users.noreply.github.com>
2022-07-31 14:14:47 -0500

    Update reinstall-rapiddisk-c.sh

--- scripts/reinstall-rapiddisk-c.sh
+++ scripts/reinstall-rapiddisk-c.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # For Debian: v1.01
-# Reinstall Part3/3 for Rapiddisk 8.2.0 Linux.
+# Reinstall Part 3/3 for Rapiddisk 8.2.0 Linux.
 # Everytime there is a Kernet change, Rapiddisk needs to be reinstalled.
 # To execute: save and `chmod +x ./reinstall-linux-c.sh` then `./reinstall-rapiddisk-c.sh`
 
