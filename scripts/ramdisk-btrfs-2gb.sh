ddd512871a03e429a98fe2b7adb8e3edf8c233b8
juanpc2018 <33388902+juanpc2018@users.noreply.github.com>
2022-07-31 14:19:29 -0500

    Create ramdisk-btrfs-2gb.sh

---
+++ scripts/ramdisk-btrfs-2gb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# For Debian: v1.01
+# Btrfs = Ultra safe, but has too much Journaling = 1.7GB Free.
+# Better than NTFS, allows special characters like: ?
+# Btrfs is Unknown with very large drives: 18TB, 20TB.
+# To execute: save and `chmod +x ./ramdisk-xfs-2gb.sh` then `./ramdisk-xfs-2gb.sh`
+
+cd ~
+sudo rapiddisk -a 2000
+sudo mkfs.btrfs -f /dev/rd0
+mkdir ramdisk
+sudo mount -t btrfs /dev/rd0 ramdisk
+sudo chown -R $(whoami):$(id -g -n) ramdisk
+
+# chown is needed because sudo creates a sudo:sudo ramdisk 
+# and $(whoami):$(id -g -n) does Not have Permissions to Write.
+# sudo is required for rapiddisk.
