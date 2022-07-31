53eb5f25298b977f5a1187cd43a29b6b809f7dff
juanpc2018 <33388902+juanpc2018@users.noreply.github.com>
2022-07-31 14:11:33 -0500

    Create ramdisk-unmap.sh

---
+++ scripts/ramdisk-unmap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# For Debian: v1.01
+# To execute: save and `chmod +x ./ramdisk-unmap.sh` then `./ramdisk-unamp.sh`
+# you need to know what is the Mapped cache drive.
+# EDIT sd?# BEFORE Running.
+
+cd ~
+# $ sudo rapiddisk -l
+# $ sudo rapiddisk -s
+sudo rapiddisk -u rc-wt_sd?#
+# $ sudo rapiddisk -d rd0
