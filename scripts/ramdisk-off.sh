3dc253bb6de745786a9400d0bd565c687bd63e31
juanpc2018 <33388902+juanpc2018@users.noreply.github.com>
2022-07-31 14:10:59 -0500

    Create ramdisk-off.sh

---
+++ scripts/ramdisk-off.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# For Debian: v1.01
+# To execute: save and `chmod +x ./ramdisk-detach.sh` then `./ramdisk-detach.sh`
+# Unmap & Unmount first.
+
+cd ~
+# $ sudo rapiddisk -l
+sudo rapiddisk -d rd0
