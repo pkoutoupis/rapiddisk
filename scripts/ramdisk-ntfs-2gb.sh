950aa20cb749b5501f0af9c1e31f8e62de89aeef
juanpc2018 <33388902+juanpc2018@users.noreply.github.com>
2022-07-31 14:16:04 -0500

    Create ramdisk-ntfs-2gb.sh

---
+++ scripts/ramdisk-ntfs-2gb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# For Debian: v1.01
+# NTFS driver is called NTFS-3g, Not to be confused with 2GB size. 
+# sometimes a necesary evil.
+# To execute: save and `chmod +x ./ramdisk-ntfs-2gb.sh` then `./ramdisk-ntfs-2gb.sh`
+
+# systemctl status rapiddiskd.service 
+cd ~
+sudo rapiddisk -a 2000
+sudo mkfs.ntfs /dev/rd0
+mkdir ramdisk
+sudo mount -t ntfs /dev/rd0 ramdisk
+sudo chown -R $(whoami):$(id -g -n) ramdisk
